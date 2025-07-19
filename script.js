942bc890d803eda1f23da767743098397018da2f
Divya-bloom <Divyagautam.official@gmail.com>
2025-07-19 23:20:34 +0530

    Add files via upload
    Initial upload of Bloom by Divya website

---
+++ script.js
@@ -0,0 +1,11 @@
+// 🌄 Slideshow Auto-Changer
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showNextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(showNextSlide, 4000); // Change every 4 seconds
